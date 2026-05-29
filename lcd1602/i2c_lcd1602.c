0855fd6801ee19f6d31e9496dc7fd29c135eaf16
Mila Cerny <neoforfunn@gmail.com>
2026-05-29 13:48:13 +0200

    feat: create lcd_init function

--- lcd1602/i2c_lcd1602.c
+++ lcd1602/i2c_lcd1602.c
@@ -5,6 +5,8 @@
 #define LCD_EN          0x04
 #define LCD_BACKLIGHT   0x08
 
+#define LCD_DELAY_MS(ms) vTaskDelay((ms) / portTICK_PERIOD_MS)
+
 static void lcd_send_nibble(i2c_master_dev_handle_t dev, uint8_t nibble, uint8_t rs) {
     nibble <<= 4; // Shift the nibble to the upper 4 bits
     uint8_t data = nibble | (rs ? LCD_RS : 0) | LCD_BACKLIGHT; // Set RS and backlight bits
@@ -34,7 +36,25 @@ static void lcd_send_string(i2c_master_dev_handle_t dev, const char* str) {
         lcd_send_char(dev, *str++);
     }
 }
-
 static void lcd_init(i2c_master_dev_handle_t dev) {
-    lcd_send_byte
+    // Wait for LCD to power up
+    LCD_DELAY_MS(50); 
+    // Function set: 8-bit mode
+    for(int i = 0; i < 3; i++) {
+        lcd_send_nibble(dev, 0x03, 0); 
+        LCD_DELAY_MS(5);
+    }
+    // Function set: 4-bit mode 
+    lcd_send_nibble(dev, 0x02, 0);   
+    // Function set: 4-bit mode, 2 lines, 5x8 dots
+    lcd_send_command(dev, 0x28); 
+    // Display off
+    lcd_send_command(dev, 0x08); 
+    // Clear display
+    lcd_send_command(dev, 0x01);
+    LCD_DELAY_MS(5); 
+    // Entry mode - 4 bit
+    lcd_send_command(dev, 0x06); 
+    // Display on, cursor off, blink off
+    lcd_send_command(dev, 0x0C);    
 }
